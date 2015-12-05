b25ac6138a6b6487b4fc1dc682b86cb1c872b5f3
Davide De Franceschi <DeFrenZ@defroBook-Pro-Retina.local>
2015-12-05 16:04:42 +0000

    Add CocoaPods with Pusher

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Clover Crash' do
+
+pod 'libPusher', '~> 1.6.1'
+
+end
+
---
+++ Pods/Target Support Files/Pods-Clover Crash/Pods-Clover Crash-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Clover_CrashVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Clover_CrashVersionString[];
+
